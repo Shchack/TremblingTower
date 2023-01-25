d6e9b8168a37ed237e98a2695403fc98af2a4132
SOLO\newer <newerainua@gmail.com>
2023-01-25 15:22:25 +0200

    Add URP. Create demo hex map

---
+++ Assets/Features/HexMap/Scripts/MapTile.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class MapTile : MonoBehaviour
+{
+    [SerializeField] private Renderer _renderer;
+
+
+    void OnMouseOver()
+    {
+        Debug.Log("Pointer over object!");
+
+        _renderer.material.EnableKeyword("_EMISSION");
+    }
+
+    void OnMouseExit()
+    {
+        _renderer.material.DisableKeyword("_EMISSION");
+    }
+}
