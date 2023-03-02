8d84190fb74e3c5b3f8c3a08d7f511dddc4f91ae
SOLO\newer <newerainua@gmail.com>
2023-03-02 17:38:14 +0200

    Add basic Pause screen

---
+++ Assets/Scripts/Game/PauseScreen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EG.Tower.Game
+{
+    public class PauseScreen : MonoBehaviour
+    {
+        [SerializeField] private Canvas _canvas;
+        [SerializeField] private Button _continueButton;
+        [SerializeField] private Button _exitButton;
+
+        private void Awake()
+        {
+            _canvas.enabled = false;
+            _continueButton.onClick.AddListener(ContinueGame);
+            _exitButton.onClick.AddListener(ExitGame);
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                ToggleCanvasView();
+            }
+        }
+
+        private void ExitGame()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#endif
+            Application.Quit();
+        }
+
+        private void ContinueGame()
+        {
+            _canvas.enabled = false;
+
+            if (!_canvas.enabled)
+            {
+                Time.timeScale = 1f;
+            }
+        }
+
+        private void ToggleCanvasView()
+        {
+            _canvas.enabled = !_canvas.enabled;
+
+            Time.timeScale = !_canvas.enabled ? 0f : 1f;
+        }
+    }
+}
